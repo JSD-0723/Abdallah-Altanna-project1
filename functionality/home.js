51191ce6b74fef64540f3991fceed29b9d7811a1
abdallahaltanna <abdallahmyaltanna@gmail.com>
2023-08-16 15:58:43 +0300

    Change theme button icon based on current theme

--- functionality/home.js
+++ functionality/home.js
@@ -9,18 +9,39 @@ function getThemeMode() {
   return localStorage.getItem(themeKey);
 }
 
+function themeBtnContent(mode) {
+  if (mode === 'dark') {
+    toggleModeBtn.innerHTML = `
+    <ion-icon name="sunny-outline" class="header_actions--icon"></ion-icon>
+    <span>Light Mode</span>
+    `;
+  } else if (mode === 'light') {
+    toggleModeBtn.innerHTML = `
+    <ion-icon
+    name="moon-outline"
+    class="header_actions--icon"
+  ></ion-icon>
+    <span>Dark Mode</span>
+    `;
+  }
+}
+
 const savedThemeMode = getThemeMode();
 if (savedThemeMode === 'dark') {
+  themeBtnContent('dark');
   document.documentElement.classList.add('dark');
 } else if (savedThemeMode === 'light') {
+  themeBtnContent('light');
   document.documentElement.classList.add('light');
 } else {
   if (
     window.matchMedia &&
     window.matchMedia('(prefers-color-scheme: dark)').matches
   ) {
+    themeBtnContent('dark');
     document.documentElement.classList.add('dark');
   } else {
+    themeBtnContent('light');
     document.documentElement.classList.add('light');
   }
 }
@@ -29,10 +50,12 @@ function toggleDarkMode() {
   if (document.documentElement.classList.contains('light')) {
     document.documentElement.classList.remove('light');
     document.documentElement.classList.add('dark');
+    themeBtnContent('dark');
     setThemeMode('dark');
   } else if (document.documentElement.classList.contains('dark')) {
     document.documentElement.classList.remove('dark');
     document.documentElement.classList.add('light');
+    themeBtnContent('light');
     setThemeMode('light');
   }
 }
